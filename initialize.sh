01988f8738a5a8f6cadfb30c6b9e00c5248b026b
1s0m0rph <Isomorph@protonmail.com>
2020-07-27 12:50:00 -0600

    make the mqtt subscriber run in background

--- initialize.sh
+++ initialize.sh
@@ -5,4 +5,4 @@ sudo apt-get install -y mosquitto mosquitto-clients
 pip3 install --user -U paho-mqtt
 
 #subscribers
-mosquitto_sub -h localhost -v -t test_topic
+mosquitto_sub -h localhost -v -t test_topic &
